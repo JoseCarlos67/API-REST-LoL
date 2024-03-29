51d951d488067b9fdbb7e67edcd0635476863332
JoseCarlos67 <carlos6767jose@gmail.com>
2024-03-28 21:17:49 -0300

    Integracao das APIs da OpenAI ChatGPT e do Google Gemini

--- src/main/java/me/dio/sdw24/application/AskChampionUseCase.java
+++ src/main/java/me/dio/sdw24/application/AskChampionUseCase.java
@@ -3,14 +3,22 @@ package me.dio.sdw24.application;
 import me.dio.sdw24.domain.exception.ChampionNotFoundException;
 import me.dio.sdw24.domain.model.Champion;
 import me.dio.sdw24.domain.ports.ChampionsRepository;
+import me.dio.sdw24.domain.ports.GenerativeAiService;
 
-public record AskChampionUseCase(ChampionsRepository repository) {
+public record AskChampionUseCase(ChampionsRepository repository, GenerativeAiService generativeAiService) {
 
     public String askChampion(Long championId, String question){
         Champion champion = repository.findByID(championId)
                 .orElseThrow(() -> new ChampionNotFoundException(championId));
 
-        String championContext = champion.generateContextByQuestion(question);
-        return championContext;
+        String context = champion.generateContextByQuestion(question);
+        String objective = """
+                Atue como um assistente com a habilidade de se comportar como os Campeões do League of Legends (LOL).
+                Responda perguntas incorporando a personalidade e estilo de um determinado Campeão.
+                Segue a pergunta, o nome do Campeão e sua respectiva lore (história):
+                 
+                """;
+
+        return generativeAiService.generateContent(objective, context);
     }
 }
